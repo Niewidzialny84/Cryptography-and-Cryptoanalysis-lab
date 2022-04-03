3837af1836704fa7151ca09252f1096ba575352b
Niewidzialny84 <niewidzialny84@vp.pl>
2022-04-03 16:42:25 +0200

    SH3 unfinished

---
+++ lab3/main.py
@@ -0,0 +1,214 @@
+from asyncio.log import logger
+import logging
+from math import log2
+
+#logging configuration 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+import numpy as np
+from bitarray import bitarray
+
+# Sha-3 algorithm
+class SHA3():
+    # Initialize the class
+    def __init__(self) -> None:
+        logging.info("Initializing SHA3 class")
+
+        pass
+        
+    # Convert S[w*(5y+x)+z] bitarray to A[x,y,z] numpy array
+    def __convert_to_A(self, S: np.ndarray, w: int) -> np.ndarray:
+        logging.info("Converting to A")
+        A = np.zeros((5, 5, w), dtype=np.uint8)
+        for x in range(5):
+            for y in range(5):
+                for z in range(w):
+                    A[x, y, z] = S[w*(5*y+x)+z]
+        return A
+
+    # Convert A[x,y,z] numpy array to S[w*(5y+x)+z] bitarray
+    def __convert_to_S(self, A: np.ndarray, w: int) -> bitarray:
+        logging.info("Converting to S")
+        S = bitarray()
+        for x in range(5):
+            for y in range(5):
+                for z in range(w):
+                    S.append(A[x, y, z])
+        return S
+
+    # Keccak theta function
+    def __theta(self, A: np.ndarray, w: int) -> np.ndarray:
+        logging.info("Theta function")
+        
+        # Create C[x,z] = A[x,0,z] ^ A[x,1,z] ^ A[x,2,z] ^ A[x,3,z] ^ A[x,4,z] numpy array 
+        C = np.zeros((5, w), dtype=np.uint8)
+        for x in range(5):
+            for z in range(w):
+                C[x, z] = A[x, 0, z] ^ A[x, 1, z] ^ A[x, 2, z] ^ A[x, 3, z] ^ A[x, 4, z]
+        
+        # Create D[x,z] = C[(x-1)%5,z] ^ C[(x+1)%5,(z-1)%w] numpy array
+        D = np.zeros((5, w), dtype=np.uint8)
+        for x in range(5):
+            for z in range(w):
+                D[x, z] = C[(x-1)%5, z] ^ C[(x+1)%5, (z-1)%w]
+        
+        # Create A'[x,y,z] = A[x,y,z] ^ D[x,z] numpy array
+        A_prime = np.zeros((5, 5, w), dtype=np.uint8)
+        for x in range(5):
+            for y in range(5):
+                for z in range(w):
+                    A_prime[x, y, z] = A[x, y, z] ^ D[x, z]
+        
+        return A_prime
+
+    # Keccak rho function
+    def __rho(self, A: np.ndarray, w: int) -> np.ndarray:
+        logging.info("Rho function")
+        
+        # Create A'[x,y,z] numpy array using algorithm
+        # For all z such that 0 <= z < w let A'[0,0,z] = A[0,0,z]
+        # Let (x,y) = (1,0)
+        # For t from 0 to 23:
+        #     for all z such that 0 <= z < w let A'[x,y,z] = = A[x,y,(z-(t+1)(t+2))]
+        #     Let (x,y) = (y, (2*x+3*y)%5)
+
+        A_prime = np.zeros((5, 5, w), dtype=np.uint8)
+        A_prime[0, 0, :] = A[0, 0, :]
+        x ,y = 1, 0
+        for t in range(24):
+            for z in range(w):
+                A_prime[x, y, z] = A[x, y, (z-(t+1)*(t+2))%w]
+            x, y = y, (2*x+3*y)%5
+
+        return A_prime
+
+    # Keccak pi function
+    def __pi(self, A: np.ndarray, w: int) -> np.ndarray:
+        logging.info("Pi function")
+
+        # For all triplets (x,y,z) such that 0 <= x < 5 and 0 <= y < 5 and 0 <= z < w let A'[x,y,z] = A[(x+3y)%5,x,z])]
+        A_prime = np.zeros((5, 5, w), dtype=np.uint8)
+        for x in range(5):
+            for y in range(5):
+                for z in range(w):
+                    A_prime[x, y, z] = A[(x+3*y)%5, x, z]
+
+        return A_prime
+    
+    # Keccak chi function
+    def __chi(self, A: np.ndarray, w: int) -> np.ndarray:
+        logging.info("Chi function")
+
+        # For all triplets (x,y,z) such that 0 <= x < 5 and 0 <= y < 5 and 0 <= z < w let A'[x,y,z] = A[x,y,z] ^ ((~A[(x+1)%5,y,z]) & A[(x+2)%5,y,z])
+        A_prime = np.zeros((5, 5, w), dtype=np.uint8)
+        for x in range(5):
+            for y in range(5):
+                for z in range(w):
+                    A_prime[x, y, z] = A[x, y, z] ^ ((~A[(x+1)%5, y, z]) & A[(x+2)%5, y, z])
+
+        return A_prime
+
+    # Keccak iota function
+    def __iota(self, A: np.ndarray, w: int, i: int) -> np.ndarray:
+        logging.info("Iota function")
+
+        # For all triplets (x,y,z) such that 0 <= x < 5 and 0 <= y < 5 and 0 <= z < w let A'[x,y,z] = A[x,y,z]
+        # Let RC = 0^w
+        # For j from 0 to  l let RC[(2**j) - 1] = rc(j+(7*i))
+        # For all z such that 0 <= z < w let A'[0,0,z] = A[0,0,z] ^ RC[z]
+
+        A_prime = np.zeros((5, 5, w), dtype=np.uint8)
+        for x in range(5):
+            for y in range(5):
+                for z in range(w):
+                    A_prime[x, y, z] = A[x, y, z]
+
+        # rc(t) is computed as follows:
+        # If t mod 255 == 0 then rc[t] = 1
+        # Let R = 10000000
+        # For i from 1 to t mod 255, let:
+        #     R = 0 || R
+        #     R[0] = R[0] ^ R[8]
+        #     R[4] = R[4] ^ R[8]
+        #     R[5] = R[5] ^ R[8]
+        #     R[6] = R[6] ^ R[8]
+        #     R = Trunc8[R]
+        # return R[0]
+        def rc(t: int) -> int:
+            if t % 255 == 0:
+                return 1
+            R = bitarray('10000000')
+            for i in range(t % 255):
+                R.append(False)
+                R[0] = R[0] ^ R[8]
+                R[4] = R[4] ^ R[8]
+                R[5] = R[5] ^ R[8]
+                R[6] = R[6] ^ R[8]
+                R = R[:8]
+            return R[0]
+
+        RC = np.zeros(w, dtype=np.uint8)
+        for j in range(int(log2(w))):
+            RC[(2**j) - 1] = rc(j+(7*i))
+        
+        A_prime[0, 0, :] = A[0, 0, :] ^ RC  
+
+        return A_prime
+
+    # Keccak-p function for string S - Keccak-p[b,nr](S)
+    def __keccak_p(self, b: int, nr: int, S: str) -> str:
+        logging.info("Keccap-p function")
+
+        w = int(b/25)
+
+        logging.info("b = %d, nr = %d, w = %d", b, nr, w)
+
+        # Convert string S to a numpy array A
+        A = self.__convert_to_A(S, w)
+
+        # Initialize A' to A
+        A_prime = A
+
+        # For i from 0 to nr - 1:
+        #     A = Keccak-p[b,nr](A)
+        for ir in range(nr):
+            logger.info("Iteration Keccak %d", ir)
+
+            A_prime = self.__theta(A, w)
+            A_prime = self.__rho(A_prime, w)
+            A_prime = self.__pi(A_prime, w)
+            A_prime = self.__chi(A_prime, w)
+            A_prime = self.__iota(A_prime, w, ir)
+
+        # Convert A_prime to a string S'
+        S_prime = self.__convert_to_S(A_prime,w)
+
+        return S_prime
+
+    # Sha 3-256 execution
+    @staticmethod
+    def sha3_256(message: str) -> str:
+        logging.info("Executing sha3-256 on message: {}".format(message))
+        
+        # Initialize class
+        sha3 = SHA3()
+
+        test_S = bitarray('0110001010101110001010101010101101011011101010000101100011010011111000101010111000101010101010110101101110101000010110001101001111100010101011100010101010101011010110111010100001011000110100111110001010101110001010101010101101011011101010000101100011010011111000101010111000101010101010110101101110101000010110001101001111100010101011100010101010101011010110111010100001011000110100111110001010101110001010101010101101011011101010000101100011010011111000101010111000101010101010110101101110101000010110001101001111100010101011100010101010101011010110111010100001011000110100111110001010101110001010101010101101011011101010000101100011010011111000101010111000101010101010110101101110101000010110001101001111100010101011100010101010101011010110111010100001011000110100111110001010101110001010101010101101011011101010000101100011010011111000101010111000101010101010110101101110101000010110001101001111100010101011100010101010101011010110111010100001011000110100111110001010101110001010101010101101011011101010000101100011010011111000101010111000101010101010110101101110101000010110001101001111100010101011100010101010101011010110111010100001011000110100111110001010101110001010101010101101011011101010000101100011010011111000101010111000101010101010110101101110101000010110001101001111100010101011100010101010101011010110111010100001011000110100111110001010101110001010101010101101011011101010000101100011010011111000101010111000101010101010110101101110101000010110001101001111100010101011100010101010101011010110111010100001011000110100111110001010101110001010101010101101011011101010000101100011010011')
+
+        X = sha3.__keccak_p(b=1600, nr=24, S=test_S)
+
+        logger.info("X: {}".format(X))
+
+        return None
+
+
+def main():
+    logging.info("Main")
+    
+    message = "Hello World!"
+    logging.info("Message: {}".format(message))
+    result = SHA3.sha3_256(message)
+    logging.info("Result: {}".format(result))
+
+if __name__ == "__main__":
+    main()
